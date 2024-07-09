303feef58b971a864cecaa8057ee76c57c91ffbf
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-09 15:45:27 -0700

    Update items_routes.js

--- routes/home/items_routes.js
+++ routes/home/items_routes.js
@@ -5,6 +5,7 @@ const itemsControoler = require('../../controllers/home/items_controller');
 const { verifyAdmin } = require('../../utility/verify_role_admin');
 const multer = require('multer');
 const { verifyVendor } = require('../../utility/verify_role_vendor');
+const { verifyAdd } = require('../../utility/verify_add');
 router.get('/getAllItems',verifyToken,itemsControoler.getAllItems);
 router.get('/getSearchItem',verifyToken,itemsControoler.getSearchItems);
 router.get('/getLikesItem',verifyToken,itemsControoler.getLikesItems);
@@ -14,10 +15,11 @@ router.get('/admin/getLatestItemItemsVerify',verifyToken,verifyAdmin,itemsContro
 router.get('/admin/getLatestItemItemsNotVerify',verifyToken,verifyAdmin,itemsControoler.getLatestItemsNotVerifyAdmin);
 router.delete('/admin/deleteItem',verifyToken,verifyAdmin,itemsControoler.deleteItemAdmin);
 router.patch('/admin/changeStatusVerify',verifyToken,itemsControoler.changeItemStatusAdmin);
-router.post('/addImage',verifyToken,verifyVendor,itemsControoler.iUpload.single('image'),itemsControoler.addImage);
-router.post('/addProduct',verifyToken,verifyVendor,itemsControoler.addProduct);
+router.post('/addImage',verifyToken,verifyVendor,verifyAdd,itemsControoler.iUpload.single('image'),itemsControoler.addImage);
+router.post('/addProduct',verifyToken,verifyVendor,verifyAdd,itemsControoler.addProduct);
 router.get('/vendor/getLatestItemItemsVerify',verifyToken,verifyVendor,itemsControoler.getLatestItemsVerifyVendor);
 router.get('/vendor/getLatestItemItemsNotVerify',verifyToken,verifyVendor,itemsControoler.getLatestItemsNotVerifyVendor);
 router.delete('/vendor/deleteItem',verifyToken,verifyVendor,itemsControoler.deleteItemVendor);
+router.get('/vendor/verifyAdd',verifyToken,verifyVendor,itemsControoler.verifyAddP);
 module.exports = 
 router
