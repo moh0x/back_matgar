a9decd35f18b4f0e9c793cb782c13c557ade7126
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-16 04:01:55 -0700

    Update adress_routes.js

--- routes/adress/adress_routes.js
+++ routes/adress/adress_routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const adressController = require('../../controllers/adress/adress_controller');
 const { verifyToken } = require("../../utility/verify_token");
-router.get('/myAdreses',verifyToken,adressController.getMyAdresses);
-router.post('/addAdress',verifyToken,adressController.addAdress);
-router.delete('/deleteAdress',verifyToken,adressController.deleteAdress);
+const { verifyUser } = require('../../utility/verify_role_user');
+router.get('/myAdreses',verifyToken,verifyUser,adressController.getMyAdresses);
+router.post('/addAdress',verifyToken,verifyUser,adressController.addAdress);
+router.delete('/deleteAdress',verifyToken,verifyUser,adressController.deleteAdress);
   module.exports = 
     router
