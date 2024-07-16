c6671951370683acd85a3b90dc1eced204b0e90e
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-16 04:01:05 -0700

    Update adress_model.js

--- models/adress/adress_model.js
+++ models/adress/adress_model.js
@@ -1,27 +1,33 @@
 const mongoose = require("mongoose");
 const adressSchema = new mongoose.Schema({
+    adressName:{
+        type:String,
+        required:true,
+       
+        maxLength:30
+    },
     adressCity:{
         type:String,
         required:true,
-        minLength:3,
+        
         maxLength:30
     },
     adressMiniCipality:{
         type:String,
         required:true,
-        minLength:3,
+      
         maxLength:30
     },
     adressDistrict:{
         type:String,
         required:true,
-        minLength:3,
+        
         maxLength:50
     },
     adressUserId:{
         type:String,
         required:true,
-        minLength:3,
+    
         maxLength:30
     },
    
